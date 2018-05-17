9f2e859527fa08364bc79fd5a56bc1fb146df989
BRUNO ROCHA PREDOLIM <bruno.rpredolim@sp.senac.br>
2018-05-17 18:52:35 -0300

    Menu responsivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  showFiller = false;
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+
+  ngOnInit () {  }
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+  
 }
