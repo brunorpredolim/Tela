9156e65fa22b6bb9c235a20c35f3c447edae1086
bruno predolim <brunopredolim@MacBookBruno.local>
2018-05-19 13:30:43 -0300

    novo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public show:boolean = false;
-  public buttonName:any = 'Show';
-  public  mostraMenu : string = "menu";
- 
-  ngOnInit () {  }
-
-  toggle() {
-    this.show = !this.show;
-   
-    
-    // CHANGE THE NAME OF THE BUTTON.
-    if(this.show)  
-    {
-      this.buttonName = "Hide";
-      this.mostraMenu = "call_received";
-    } 
-    else
-    {
-      this.buttonName = "Show";
-      this.mostraMenu = "menu";
-    }
-  }
-  
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import 'hammerjs';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatMenuModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatCardModule,
-        MatGridListModule} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatButtonModule,
-    BrowserAnimationsModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatCardModule,
-    MatGridListModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
